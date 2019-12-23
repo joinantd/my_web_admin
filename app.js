8a6ad621154c17c7782bba9ea78627cc5b890a29
lxy <945663638@qq.com>
2019-12-23 22:31:19 +0800

    koa-router

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 let koa = require("koa");
-let bodyParser = require("koa-bodyparser");
-let app = new koa();
-app.use(bodyParser());
+let router = require("koa-router")(); //引入并实例化
 
-const router = require("koa-router")({
-  prefix: "/api"
+let app = new koa();
+// let router = new Router();  手动实例化
+
+//ctx 上下文 content, 包含了request response等信息
+//配置路由
+router.get("/", async (ctx) => {
+  ctx.body = "首页"; //返回数据
+}).get('/newscotent', async(ctx) => {
+    ctx.body = '这是一个新闻详情页面'
 });
-router.get("/", async (ctx, next) => {
-  // todo
-});
-app.use(router.routes());
+
+
+
+
+//获取get传值
+router.get("/", async(ctx) => {
+
+    console.log(ctx.query) //获取的是对象
+    console.log(ctx.querystring)
+
+    //从ctx中的request里面获取get值
+    console.log(ctx.request)
+    //获取url地址
+    console.log(ctx.url)
+    ctx.body = '这是一个新闻页面'
+
+})
+
+
+//启动路由
+app
+  .use(router.routes()) //启动路由
+//   .use(router.alloweMethods());
 
 app.listen(3000, () => {
   console.log("888888888888");
