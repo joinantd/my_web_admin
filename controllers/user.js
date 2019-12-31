037f3cf65b4041a61b3160989c9082aa86ddda42
lxy <945663638@qq.com>
2019-12-31 15:54:47 +0800

    koa-session

--- controllers/user.js
+++ controllers/user.js
@@ -6,21 +6,35 @@ class UserControllers {
       username: ctx.request.body.username,
       password: ctx.request.body.password
     };
-    if (ctx.request.body.username) {
+    if (ctx.request.body.username && ctx.request.body.password) {
       try {
-        let data = await UserService.login(users.username);
-        ctx.response.status = 200;
-        ctx.body = {
-          code: 200,
-          msg: "查询成功",
-          data: data[0]
-        };
+        // let data = await UserService.login(users.username);
+        await UserService.login(users.username).then(data => {
+          if (
+            !!data.length &&
+            data[0].username === users.username &&
+            data[0].password === users.password
+          ) {
+            ctx.response.status = 200;
+            ctx.body = {
+              code: 200,
+              msg: "查询成功",
+              data: data[0]
+            };
+          } else {
+            ctx.response.status = 201;
+            ctx.body = {
+              code: 201,
+              msg: "用户名或密码错误",
+              data: []
+            };
+          }
+        });
       } catch (err) {
         ctx.response.status = 412;
         ctx.body = {
           code: 412,
-          msg: "查询失败",
-          data
+          msg: "查询失败"
         };
       }
     } else {
